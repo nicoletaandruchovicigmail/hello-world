d4e2511017b30fd698dd90a97a09c06b4dcf8cd3
Nicoleta Andruchovici <nicoleta.andruchovici@gmail.com>
2017-06-12 13:15:32 +0200

    test initial commit hope this works

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test
+            //test 1
+            //test 2
+            //test 3
+            //test 4
+        }
+    }
+}
